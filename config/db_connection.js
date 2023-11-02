e8c550e7a45334361c1cb0055e33d63d876313a0
aryan <aryansharma2003march@gmail.com>
2023-11-02 11:54:31 +0530

    updated

--- config/db_connection.js
+++ config/db_connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const {env} = process;
 // Create a new Sequelize instance with the dialect set to 'postgres'
 const connectDB = () => {
-    const db = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASS, {
+    const db = new Sequelize("ats", env.DB_USER, env.DB_PASS, {
         host: env.DB_HOST,
         dialect: env.DB_DIALECT,
         pool: {
