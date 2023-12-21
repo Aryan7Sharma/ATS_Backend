e847668cc97a3d6d1e3444e0d802e1d9dd9198ea
aryan <aryansharma2003march@gmail.com>
2023-12-21 14:23:29 +0530

    updated(dec21)

--- middlewares/express_validator/validateBody.js
+++ middlewares/express_validator/validateBody.js
@@ -1,13 +1,14 @@
-const {validationResult} = require('express-validator');
+const { validationResult } = require('express-validator');
 
 const validateBody = async (req, res, next) => {
-    //console.log(req.body);
-    const errors = await validationResult(req);
-    //console.log("inside validate body");
-    if (!errors.isEmpty()) {
-      return res.status(422).json(errors.array());
-    } else {
-      next();
+  //console.log(req.body);
+  const errors = await validationResult(req);
+  //console.log("inside validate body");
+  if (!errors.isEmpty()) {
+    logger.error(`Error in Req Body Validation ${errors}`);
+    return res.status(422).json(errors.array());
+  } else {
+    next();
   }
 }
 
